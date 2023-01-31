e2198206f96617e23ed9963c2b3e7116d4723475
votrepoesie <vrli@uchicago.edu>
2023-01-31 10:31:51 -0600

    update (nick's comments)

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const bgColor = 'black'
-const gifURL = 'heart.gif'
+const gifURL = 'blinkingheart.gif'
 const gifOffsetX = 75
 const gifOffsetY = 75
   
@@ -22,17 +22,21 @@ function createGifCircle (num, scale, radius) {
     }
 }
   
-function setup () {
+function animate () {
+
+    setTimeout(animate, 3000)
+    document.body.innerHTML = ''
+
+
     document.body.style.backgroundColor = bgColor
     document.body.style.overflow = 'hidden'
+
+
     createGifCircle(30, 0.3, 100)
     setTimeout(createGifCircle, 100, 30, 0.3, 150)
     setTimeout(createGifCircle, 200, 30, 0.3, 160)
-    // setTimeout(createGifCircle, 250, 30, 0.3, 190)
     setTimeout(createGifCircle, 300, 30, 0.3, 180)
-    // setTimeout(createGifCircle, 300, 30, 0.3, 230)
     setTimeout(createGifCircle, 400, 30, 0.3, 250)
-    // setTimeout(createGifCircle, 350, 30, 0.3, 270)
     setTimeout(createGifCircle, 500, 30, 0.3, 300)
     setTimeout(createGifCircle, 600, 100, 1, 360)
     createGifCircle(1, 0.6, 1)
@@ -41,21 +45,17 @@ function setup () {
 
 const changeColor = () => {
     console.log('hello')
-    const r = Math.random() * 255
-    const g = Math.random() * 255
-    const b = Math.random() * 255
-    const color = `rgb(${r - 150}, ${g - 150}, ${b - 150})`
+    const h = Math.random() * 360
+    const s = 25 + 70 * Math.random() + '%'
+    const l = 90 + '%'
+    const color = `hsl(${h}, ${s}, ${l})`
     document.body.style.backgroundColor = color
     setTimeout(changeColor, 500)
 }
  
-window.addEventListener('load', setup)
+window.addEventListener('load', animate)
 
 changeColor()
-
-setTimeout(() => {
-    document.location.reload();
-}, 3000);
 
 
 
