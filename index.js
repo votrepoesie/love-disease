37de120304ca89fedd36101a5c510e2b5bd50069
votrepoesie <vrli@uchicago.edu>
2023-01-31 11:34:21 -0600

    divident

--- index.js
+++ index.js
@@ -10,14 +10,16 @@ function createGif (x, y) {
     gif.style.position = 'absolute'
     gif.style.left = x + 'px'
     gif.style.top = y + 'px'
+    gif.style.filter.saturate = 0.5
 }
   
 function createGifCircle (num, scale, radius) {
     for (let i = 0; i < num; i++) {
       const cx = window.innerWidth / 2 - gifOffsetX
       const cy = window.innerHeight / 2 - gifOffsetY
-      const x = cx + Math.sin(i * scale) * radius
-      const y = cy + Math.cos(i * scale) * radius
+      const divident = Math.random() + 0.7
+      const x = cx + Math.sin(i * scale) * radius / divident
+      const y = cy + Math.cos(i * scale) * radius / divident
       createGif(x, y)
     }
 }
@@ -34,11 +36,14 @@ function animate () {
 
     createGifCircle(30, 0.3, 100)
     setTimeout(createGifCircle, 100, 30, 0.3, 150)
-    setTimeout(createGifCircle, 200, 30, 0.3, 160)
-    setTimeout(createGifCircle, 300, 30, 0.3, 180)
-    setTimeout(createGifCircle, 400, 30, 0.3, 250)
-    setTimeout(createGifCircle, 500, 30, 0.3, 300)
-    setTimeout(createGifCircle, 600, 100, 1, 360)
+    setTimeout(createGifCircle, 300, 30, 0.3, 160)
+    setTimeout(createGifCircle, 500, 30, 0.3, 180)
+    setTimeout(createGifCircle, 700, 30, 0.3, 200)
+    setTimeout(createGifCircle, 900, 30, 0.3, 230)
+    setTimeout(createGifCircle, 1100, 30, 0.3, 260)
+    setTimeout(createGifCircle, 1300, 30, 0.3, 290)
+    setTimeout(createGifCircle, 1500, 30, 0.3, 320)
+    setTimeout(createGifCircle, 1700, 30, 0.3, 350)
     createGifCircle(1, 0.6, 1)
 
 }
